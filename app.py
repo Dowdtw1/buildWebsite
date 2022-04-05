96a8d497884c31ef2ac20dd001a9100c46f9311d
Dowdtw1 <103059208+Dowdtw1@users.noreply.github.com>
2022-04-05 17:00:13 +0100

    Initial Up

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+# Custom imports
+from multiapp import MultiApp
+from apps import news_content, sport_content, home # import your pages here
+
+app = MultiApp()
+
+# st.markdown("""
+# # Multi-Page App
+# This multi-page app is using the [streamlit-multiapps](https://github.com/upraneelnihar/streamlit-multiapps) framework developed by [Praneel Nihar](https://medium.com/@u.praneel.nihar). Also check out his [Medium article](https://medium.com/@u.praneel.nihar/building-multi-page-web-app-using-streamlit-7a40d55fa5b4).
+# """)
+
+# Add all your application here
+app.add_app("News", news_content.app)
+app.add_app("Sports", sport_content.app)
+app.add_app("Home", home.app)
+
+# The main app
+app.run()
---
+++ home.py
@@ -0,0 +1,13 @@
+import streamlit.components.v1 as components
+
+
+def app():
+    components.html("""
+
+    <!-- ZEPHR_FEATURE article -->
+    <div>
+        <p>This is the home page</p>
+    </div>
+
+    <!-- ZEPHR_FEATURE_END article -->
+    """)
