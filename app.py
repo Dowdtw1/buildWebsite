af7836e0bee10b814b8f11de2b132cd5bba39748
Dowdtw1 <103059208+Dowdtw1@users.noreply.github.com>
2022-04-05 19:08:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ from apps import news_content, sport_content, home # import your pages here
 
 app = MultiApp()
 
-# st.markdown("""
-# # Multi-Page App
-# This multi-page app is using the [streamlit-multiapps](https://github.com/upraneelnihar/streamlit-multiapps) framework developed by [Praneel Nihar](https://medium.com/@u.praneel.nihar). Also check out his [Medium article](https://medium.com/@u.praneel.nihar/building-multi-page-web-app-using-streamlit-7a40d55fa5b4).
-# """)
+st.markdown("""
+# Multi-Page App
+This multi-page app is using the [streamlit-multiapps](https://github.com/upraneelnihar/streamlit-multiapps) framework developed by [Praneel Nihar](https://medium.com/@u.praneel.nihar). Also check out his [Medium article](https://medium.com/@u.praneel.nihar/building-multi-page-web-app-using-streamlit-7a40d55fa5b4).
+""")
 
 # Add all your application here
 app.add_app("News", news_content.app)
