f4a93aa1e267d27dd30dfc240b0afa7fa94f40c4
Dowdtw1 <103059208+Dowdtw1@users.noreply.github.com>
2022-04-05 17:03:13 +0100

    Add files via upload

---
+++ apps/news_content.py
@@ -0,0 +1,14 @@
+
+import streamlit.components.v1 as components
+
+
+def app():
+    components.html("""
+
+    <!-- ZEPHR_FEATURE article -->
+    <div>
+        <p>This is the news content page</p>
+    </div>
+
+    <!-- ZEPHR_FEATURE_END article -->
+    """)
